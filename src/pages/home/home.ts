90fafe8ba39fab1fd08b3d93b35c05822aaa3111
myasura <sasura@ptnicec.ac.th>
2018-11-08 14:42:16 +0700

    send project

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheetController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public acSheet: ActionSheetController,
+    public navCtrl: NavController) {
 
+  }
+
+  showAction(){
+     const action = this.acSheet.create({
+       title: 'เลือกรายการ',
+       subTitle:'---------------------',
+       buttons:[
+       {
+         text:'Add'
+       },
+       {
+         text:'Edit',
+         role:'edit',
+         handler: () => {
+           console.log('แก้ไขแล้ว')
+         }
+       },
+        {
+          text:'DELETE',
+          role:'edit',
+          handler: () => {
+            console.log('ลบเรียบร้อยแล้ว')
+          }
+        }
+      ]
+     });
+     action.present();
   }
 
 }
